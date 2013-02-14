5fb736274a82c1ed4b58e48433a8d4cbb820c3ab
Olaf Merkert <olaf@m-merkert.de>
2013-02-14 19:05:19 +0100

    reduce boilerplate for defining basic math-output methods with macros

--- basic-math-output.lisp
+++ basic-math-output.lisp
@@ -5,8 +5,7 @@
                  princ-to-stream))
 
 (defun math-output-to-record (stream)
-  (lambda (s) (aprog1 (math-output s stream)
-           (stream-add-output-record stream it))))
+  (lambda (s) (math-output s stream)))
 
 (defun princ-to-stream (stream &optional (record-type 'operator-math-output-record))
   (lambda (s)
@@ -14,39 +13,48 @@
       (princ s stream)
       new-operator)))
 
-(defmacro define-math-output-method ((type output-record-type) &body body)
+;;; macros to take care of the boilerplate for establishing new basic types
+(defmacro define-basic-math-output-class (type slots &key centering primitive)
+  "If CENTERING, "
+  (let ((mo-classname (symb type '-math-output-record)))
+    `(progn
+       ,@(unless primitive
+                 `((defclass ,type ()
+                     ,(mapcar #`(,a1 :initarg ,(keyw a1)
+                                     :initform nil
+                                     :reader ,a1) ; TODO maybe use accessor here
+                              slots))))
+       (defclass ,mo-classname
+           (,(if centering
+                 'math-output-record/with-center
+                 'math-output-record)
+             standard-sequence-output-record
+             ,@(unless primitive
+                       (list type)))
+         ()))))
+
+(defmacro define-basic-math-output-method ((type output-record-type) &body body)
+  "Anaphoric variables: STREAM, NEW-RECORD."
   `(defmethod math-output ((,type ,type) stream)
-     (with-output-to-output-record (stream ',output-record-type new-record
+     (with-new-output-record (stream ',output-record-type new-record
                                            :math-object ,type)
-       ,@body)))
+       ,@body
+       new-record)))
 
+(defmacro define-basic-math-output ((type slots &key centering primitive) &body body)
+  `(progn
+     (define-basic-math-output-class ,type ,slots :centering ,centering :primitive ,primitive)
+     (define-basic-math-output-method (,type ,(symb type '-math-output-record))
+       ,@(if primitive body
+             `((with-slots ,slots new-record ,@body))))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; output of ordinary numbers
-(defclass number-math-output-record (math-output-record
-                                     standard-sequence-output-record)
-  ())
-
-(define-math-output-method (number number-math-output-record)
-  
+(define-basic-math-output (number nil :primitive t)
   (princ number stream))
 
 ;;; output of (explicit) fractions
-(defclass fraction ()
-  ((numerator   :initarg :numerator
-                :initform nil
-                :reader numerator)
-   (denominator :initarg :denominator
-                :initform nil
-                :reader denominator))
-  (:documentation "TODO"))
-
-(defclass fraction-math-output-record (math-output-record/with-center
-                                       standard-sequence-output-record
-                                       fraction)
-  ())
-
-(define-math-output-method (fraction fraction-math-output-record)
-  (with-slots (numerator denominator) new-record
+(define-basic-math-output (fraction (numerator denominator) :centering t)
     (setf numerator   (math-output (numerator fraction) stream)
           denominator (math-output (denominator fraction) stream))
     (stream-add-output-record stream numerator)
@@ -64,32 +72,10 @@
                     x center (+ x w) center)
         ;; mark center offset
         (setf (center-offset new-record)
-              (values (floor w 2) (- center y)))))))
+              (values (floor w 2) (- center y))))))
 
 ;;; finite sum expressions
-(defclass finite-sum ()
-  ((summands  :initarg :summands
-              :initform nil
-              :accessor summands)
-   (operators :initarg :operators
-              :initform nil
-              :accessor operators)))
-
-(defclass finite-sum-math-output-record (math-output-record
-                                         standard-sequence-output-record
-                                         finite-sum)
-  ())
-
-(defmethod initialize-instance :after ((sum finite-sum-math-output-record) &key)
-  (with-slots (summands operators) sum
-    (cond ((= (length summands) (+ 1 (length operators))))
-          ((null operators)
-           (setf operators (mapcar (constantly '+) (rest summands))))
-          (t (error "finite-sum: length of summands (~A) and operators (~A) do not match."
-                    (length summands) (length operators))))))
-
-(define-math-output-method (finite-sum finite-sum-math-output-record-sum)
-    (with-slots (summands operators) new-record
+(define-basic-math-output (finite-sum (summands operators))
     (setf summands
           (mapcar (math-output-to-record stream)
                   (summands finite-sum))
@@ -99,4 +85,22 @@
     (align-output-records (insert-operators summands operators)
                           #'stacking-align
                           #'centering-align))
-  new-record)
+
+(defmethod initialize-instance :after ((sum finite-sum) &key)
+  (unless (typep sum 'math-output-record)
+    (with-slots (summands operators) sum
+      (cond ((= (length summands) (+ 1 (length operators))))
+            ((null operators)
+             (setf operators (mapcar (constantly '+) (rest summands))))
+            (t (error "finite-sum: length of summands (~A) and operators (~A) do not match."
+                      (length summands) (length operators)))))))
+
+
+;;; finite product expressions
+(define-basic-math-output (finite-product (factors))
+  (setf factors (mapcar (math-output-to-record stream)
+                        (factors finite-product)))
+  (align-output-records factors
+                        #'stacking-align
+                        #'centering-align))
+

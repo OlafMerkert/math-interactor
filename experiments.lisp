5436f92317d57d977d217d55a341363968c2391c
Olaf Merkert <olaf@m-merkert.de>
2013-02-15 14:51:13 +0100

    disable example that were only used for primitive testing

--- experiments.lisp
+++ experiments.lisp
@@ -20,28 +20,28 @@
      (define-example% ,math-object-spec)))
 
 
-(define-example "Bruch anzeigen"
-    (fraction 1231
-              987))
+;; (define-example "Bruch anzeigen"
+;;     (fraction 1231
+;;               987))
 
-(define-example "Summe anzeigen"
-    (finite-sum (list 1 2 3 4 5 6
-                      (fraction 17 1329846))))
+;; (define-example "Summe anzeigen"
+;;     (finite-sum (list 1 2 3 4 5 6
+;;                       (fraction 17 1329846))))
 
-(define-example  "Kettenbruch anzeigen"
-    (finite-continued-fraction
-     (list 1 2 3 4 5 6 7)))
+;; (define-example  "Kettenbruch anzeigen"
+;;     (finite-continued-fraction
+;;      (list 1 2 3 4 5 6 7)))
 
-(define-example "Produkt anzeigen"
-    (finite-product (list 78 29 13)))
+;; (define-example "Produkt anzeigen"
+;;     (finite-product (list 78 29 13)))
 
-(define-example "Potenz anzeigen"
-    (finite-sum (list 8 (superscript 99 3)
-                      (fraction 8 10)
-                      (superscript (fraction 7 3) 20))))
+;; (define-example "Potenz anzeigen"
+;;     (finite-sum (list 8 (superscript 99 3)
+;;                       (fraction 8 10)
+;;                       (superscript (fraction 7 3) 20))))
 
-(define-example "Index anzeigen"
-    (subscript 99 3))
+;; (define-example "Index anzeigen"
+;;     (subscript 99 3))
 
 (define-example "Polynom anzeigen"
     (generic-math-output-implementation:math-output-prepare

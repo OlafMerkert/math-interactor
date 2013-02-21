572d9e02bfb4c9482d80f67b4199a7bec7bc07b7
Olaf Merkert <olaf@m-merkert.de>
2013-02-21 19:24:50 +0100

    disable old examples

--- experiments.lisp
+++ experiments.lisp
@@ -36,18 +36,18 @@
 ;; (define-example "Index anzeigen"
 ;;     (subscript 99 3))
 
-(define-example "Polynom anzeigen"
-    (polynomials:make-polynomial 1 -2 3 -4 5))
+;; (define-example "Polynom anzeigen"
+;;     (polynomials:make-polynomial 1 -2 3 -4 5))
 
-(define-example "Polynom 2 anzeigen"
-    (polynomials:make-polynomial 1 0 -2 3 0 -4 5))
+;; (define-example "Polynom 2 anzeigen"
+;;     (polynomials:make-polynomial 1 0 -2 3 0 -4 5))
 
- (define-example "Potenzreihe (statisch) anzeigen"
-     (power-series:make-power-series 12 1 -2 3 -4 5 32 78 0 1 7 3 81 83 1 7 89 8 7 1 7 88 8))
+;;  (define-example "Potenzreihe (statisch) anzeigen"
+;;      (power-series:make-power-series 12 1 -2 3 -4 5 32 78 0 1 7 3 81 83 1 7 89 8 7 1 7 88 8))
 
-(define-example "Potenzreihe (dynamisch) anzeigen"
-    (gm:sqrt (polynomials:make-polynomial 4 2 1 8 2 )))
+;; (define-example "Potenzreihe (dynamisch) anzeigen"
+;;     (gm:sqrt (polynomials:make-polynomial 4 2 1 8 2 )))
 
-(define-example "Kettenbruch anzeigen"
-    (make-instance 'cf-ps:continued-fraction :starting (gm:sqrt (polynomials:make-polynomial 1 0 0 1 1))))
+;; (define-example "Kettenbruch anzeigen"
+;;     (make-instance 'cf-ps:continued-fraction :starting (gm:sqrt (polynomials:make-polynomial 1 0 0 1 1))))
 ;; TODO figure out line breaking and horizontal scrolling

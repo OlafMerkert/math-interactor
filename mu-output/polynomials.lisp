c3a43a470f96be19a83ab3bb780e3995773fa6bf
Olaf Merkert <olaf@m-merkert.de>
2013-04-26 15:41:30 +0200

    make implement-printer-method invokation more readable

--- mu-output/polynomials.lisp
+++ mu-output/polynomials.lisp
@@ -3,21 +3,21 @@
 ;; create a nice output for polynomials (and series at the same
 ;; time)
 
-(implement-printer-method print-math-object math-output-printer (object)
+(implement-printer-method (print-math-object math-output-printer) (object)
   (math-output-prepare object))
 
-(implement-printer-method print-superscript math-output-printer (base exponent)
+(implement-printer-method (print-superscript math-output-printer) (base exponent)
   ;; todo may need to call math-output-prepare on these
   (superscript (math-output-prepare base) exponent))
 
-(implement-printer-method print-product math-output-printer (factors)
+(implement-printer-method (print-product math-output-printer) (factors)
   (finite-product factors))
 
-(implement-printer-method print-sum math-output-printer (summands-with-sign)
+(implement-printer-method (print-sum math-output-printer) (summands-with-sign)
   (finite-sum (mapcar #'first summands-with-sign)
               (rest (mapcar #'second summands-with-sign))))
 
-(implement-printer-method print-sum+ellipsis math-output-printer (summands-with-sign)
+(implement-printer-method (print-sum+ellipsis math-output-printer) (summands-with-sign)
   (finite-sum (append1 (mapcar #'first summands-with-sign) "...")
               (append1 (rest (mapcar #'second summands-with-sign)) '+)))
 

44d6e5b2be2b99feb23ea331917fc5a50fa6afac
Olaf Merkert <olaf@m-merkert.de>
2013-11-14 11:21:02 +0100

    functions for 2d grid alignment

--- alignment.lisp
+++ alignment.lisp
@@ -54,6 +54,13 @@
   (lambda (&rest args)
     (nth n args )))
 
+(defun adjust-output-record-position (x-combinate y-combinate)
+  (lambda (record h-pos v-pos)
+    (multiple-value-bind (x y) (output-record-position record)
+      (setf (output-record-position record)
+            (values (funcall x-combinate x h-pos)
+                    (funcall y-combinate y v-pos))))))
+
 (defun align-output-records (records horizontal-align-fn vertical-align-fn
                              &optional (x-combinate (nth-arg 1)) (y-combinate x-combinate))
   "Set the positions of the output-`records', where
@@ -64,11 +71,7 @@ and computed coordinate, so may be used to factor in current
 coordinates. Return the center of the grouping of `records'."
   (multiple-value-bind (h-pos x-off) (funcall horizontal-align-fn records :horizontal)
     (multiple-value-bind (v-pos y-off) (funcall vertical-align-fn records :vertical)
-      (mapc (lambda (record h-pos v-pos)
-              (multiple-value-bind (x y) (output-record-position record)
-                (setf (output-record-position record)
-                      (values (funcall x-combinate x h-pos)
-                              (funcall y-combinate y v-pos)))))
+      (mapc (adjust-output-record-position x-combinate y-combinate)
             records h-pos v-pos)
       ;; return center coordinates
       (values x-off y-off))))
@@ -107,3 +110,57 @@ the center-offset of the grouping of `records'."
                     lengths)
             ;; calculate the center too
             (floor (- position offset) 2))))
+
+;;; grid alignment code
+(defun grid-align (record-grid direction)
+  "Compute the positions of 2d `record-grid' (either in
+`direction' :HORIZONTAL or :VERTICAL) for grid alignment, honoring
+`*math-horizontal-spacing*' and `*math-vertical-spacing*' as
+appropriate. A second value gives the computed center-offset of the
+entire grid."
+  (dbind (rows cols) (array-dimensions record-grid)
+    (let* ((lengths (map-array1 (ecase direction
+                                  (:horizontal #'rectangle-width)
+                                  (:vertical   #'rectangle-height))
+                                record-grid))
+           (primary-lengths
+            (ecase direction
+              (:horizontal
+               (iter (for j from 0 below cols)
+                     (collect (iter (for i from 0 below rows)
+                                    (maximizing (aref lengths i j)))
+                       result-type vector)))
+              (:vertical
+               (iter (for i from 0 below rows)
+                     (collect (iter (for j from 0 below cols)
+                                    (maximizing (aref lengths i j)))
+                       result-type vector)))))
+           (position 0)
+           (offset (ecase direction
+                     (:vertical   *math-vertical-spacing*)
+                     (:horizontal *math-horizontal-spacing*)))
+           (primary-positions (map 'vector
+                                   (lambda (l)
+                                     (prog1 position
+                                       (incf position (+ l offset))))
+                                   lengths)))
+      (values (make-array/fill (rows cols) (i j)
+                (let ((primary (ecase direction
+                                 (:vertical i)
+                                 (:horizontal j))))
+                  ;; centering
+                  (+ (aref primary-positions primary)
+                     (floor (- (aref primary-lengths primary) (aref lengths i j))))))
+              (floor (- position offset) 2)))))
+
+(defun align-grid-output-records (record-grid &optional (x-combinate (nth-arg 1)) (y-combinate x-combinate))
+  "Set the positions of the output-records in `records-grid' to align
+  them nicely into a grid, and return the center of the grid. See also
+  `align-output-records'."
+  (mvbind (h-pos x-off) (grid-align record-grid :horizontal)
+    (mvbind (v-pos y-off) (grid-align record-grid :horizontal)
+      (map-array (adjust-output-record-position x-combinate y-combinate)
+                 record-grid h-pos v-pos)
+      (values x-off y-off))))
+
+;; note: grid alignment is a combination of centering and stacking alignment
